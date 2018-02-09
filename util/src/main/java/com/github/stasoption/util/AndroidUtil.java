3471733b4d77d4eb94c2ee7a39d6738ab2033caa
Stas Averin <stanislav.averin@devim.team>
2018-02-09 18:23:58 +0300

    work commit

--- util/src/main/java/com/github/stasoption/util/AndroidUtil.java
+++ util/src/main/java/com/github/stasoption/util/AndroidUtil.java
@@ -1,10 +1,18 @@
 package com.github.stasoption.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Vibrator;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public final class AndroidUtil {
@@ -46,5 +54,27 @@ public final class AndroidUtil {
         if (vibrator!=null && vibrator.hasVibrator()) {
             vibrator.vibrate(duration);
         }
+    }
+
+    public static void setStatusBarColor(@NonNull Activity activity, @ColorRes int colorRes) {
+        Window window = activity.getWindow();
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(activity, colorRes));
+        }
+    }
+
+    public static int getActionBarHeight(@NonNull Context context){
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
     }
 }
